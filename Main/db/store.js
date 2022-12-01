3cb964218040948f81335651461ca01df46d2c77
Michael Smitte <smittemichael@yahoo.com>
2022-12-01 16:48:38 -0500

    added and modified starter code

--- Main/db/store.js
+++ Main/db/store.js
@@ -7,3 +7,46 @@ const uuidv1 = require('uuid/v1');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+class Store {
+    read() {
+        return readFileAsync('./db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFileSync('./db/db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            //if notes isn't an array or can't be turned into one, send back a new empty array
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error('Note "title" and "text" cannot be blank');
+        }
+
+        //add a unique id to the note using uuid package
+        const newNote = { title, text, id: uuidv1() };
+
+        //get all notes, add the new note, write all the updated notes, return the newNote
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => notes.filter((note) => note.id !== id))
+            .then((filteredNotes) => this.write(filteredNotes));
+    }
+}
+
+module.exports = new Store();
